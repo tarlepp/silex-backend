fc285c02270f376a6f4ed20bc6d5433b04a5d772
tarlepp <tarmo.leppanen@kase.fi>
2016-03-13 13:29:19 +0200

    Added lifecycle methods to base rest controller.

--- src/App/Controllers/Interfaces/Rest.php
+++ src/App/Controllers/Interfaces/Rest.php
@@ -6,8 +6,11 @@
  */
 namespace App\Controllers\Interfaces;
 
+// Application components
+use App\Entities\Base as Entity;
+
 // Symfony components
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -31,25 +34,28 @@ interface Rest
     /**
      * Returns all specified entities from database as an array of entity objects.
      *
-     * @return  JsonResponse
+     * @param   Request $request
+     *
+     * @return  Response
      */
-    public function find();
+    public function find(Request $request);
 
     /**
      * Returns single entity object presenting given id value.
      *
-     * @param   integer $id Entity id
+     * @param   Request $request    Request object
+     * @param   integer $id         Entity id
      *
-     * @return  JsonResponse
+     * @return  Response
      */
-    public function findOne($id);
+    public function findOne(Request $request, $id);
 
     /**
      * Creates new entity object to the database.
      *
      * @param   Request $request
      *
-     * @return  JsonResponse
+     * @return  Response
      */
     public function create(Request $request);
 
@@ -59,16 +65,123 @@ interface Rest
      * @param   Request $request
      * @param   integer $id
      *
-     * @return  JsonResponse
+     * @return  Response
      */
     public function update(Request $request, $id);
 
     /**
      * Method to delete specified entity from database.
      *
+     * @param   Request $request
      * @param   integer $id
      *
-     * @return  JsonResponse
+     * @return  Response
      */
-    public function delete($id);
+    public function delete(Request $request, $id);
+
+    /**
+     * Before lifecycle method for find method.
+     *
+     * @param   Request         $request
+     * @param   array           $criteria
+     * @param   null|array      $orderBy
+     * @param   null|integer    $limit
+     * @param   null|integer    $offset
+     */
+    public function beforeFind(
+        Request $request,
+        array &$criteria = [],
+        array &$orderBy = null,
+        &$limit = null,
+        &$offset = null
+    );
+
+    /**
+     * After lifecycle method for find method.
+     *
+     * @param   Request         $request
+     * @param   array           $criteria
+     * @param   null|array      $orderBy
+     * @param   null|integer    $limit
+     * @param   null|integer    $offset
+     * @param   Entity[]        $entities
+     */
+    public function afterFind(
+        Request $request,
+        array &$criteria = [],
+        array &$orderBy = null,
+        &$limit = null,
+        &$offset = null,
+        array &$entities
+    );
+
+    /**
+     * Before lifecycle method for findOne method.
+     *
+     * @param   Request $request
+     * @param   integer $id
+     */
+    public function beforeFindOne(Request $request, &$id);
+
+    /**
+     * After lifecycle method for findOne method.
+     *
+     * @param   Request $request
+     * @param   integer $id
+     * @param   Entity  $entity
+     */
+    public function afterFindOne(Request $request, &$id, Entity $entity);
+
+    /**
+     * Before lifecycle method for create method.
+     *
+     * @param   Request     $request
+     * @param   \stdClass   $data
+     */
+    public function beforeCreate(Request $request, \stdClass $data);
+
+    /**
+     * After lifecycle method for create method.
+     *
+     * @param   Request     $request
+     * @param   \stdClass   $data
+     * @param   Entity      $entity
+     */
+    public function afterCreate(Request $request, \stdClass $data, Entity $entity);
+
+    /**
+     * Before lifecycle method for update method.
+     *
+     * @param   Request     $request
+     * @param   integer     $id
+     * @param   \stdClass   $data
+     */
+    public function beforeUpdate(Request $request, &$id, \stdClass $data);
+
+    /**
+     * After lifecycle method for update method.
+     *
+     * @param   Request     $request
+     * @param   integer     $id
+     * @param   \stdClass   $data
+     * @param   Entity      $entity
+     */
+    public function afterUpdate(Request $request, &$id, \stdClass $data, Entity $entity);
+
+    /**
+     * Before lifecycle method for delete method.
+     *
+     * @param   Request $request
+     * @param   integer $id
+     */
+    public function beforeDelete(Request $request, &$id);
+
+    /**
+     * After lifecycle method for delete method.
+     *
+     * @param   Request $request
+     * @param   integer $id
+     * @param   Entity  $entity
+     */
+    public function afterDelete(Request $request, &$id, Entity $entity);
 }
--- src/App/Controllers/Rest.php
+++ src/App/Controllers/Rest.php
@@ -8,7 +8,7 @@ namespace App\Controllers;
 
 // Application components
 use App\Services\Rest as RestService;
-use App\Entities\Base as BaseEntity;
+use App\Entities\Base as Entity;
 
 // Silex components
 use Silex\Application;
@@ -54,23 +54,49 @@ abstract class Rest extends Base implements Interfaces\Rest
     /**
      * Returns all specified entities from database as an array of entity objects.
      *
+     * @param   Request $request
+     *
      * @return  Response
      */
-    public function find()
+    public function find(Request $request)
     {
-        return $this->makeResponse($this->service->find());
+        $criteria = $request->get('criteria', []);
+        $orderBy = $request->get('orderBy', null);
+        $limit = $request->get('limit', null);
+        $offset = $request->get('offset', null);
+
+        if (method_exists($this, 'beforeFind')) {
+            $this->beforeFind($request, $criteria, $orderBy, $limit, $offset);
+        }
+
+        $entities = $this->service->find($criteria, $orderBy, $limit, $offset);
+
+        if (method_exists($this, 'afterFind')) {
+            $this->afterFind($request, $criteria, $orderBy, $limit, $offset, $entities);
+        }
+
+        return $this->makeResponse($entities);
     }
 
     /**
      * Returns single entity object presenting given id value.
      *
-     * @param   integer $id Entity id
+     * @param   Request $request    Request object
+     * @param   integer $id         Entity id
      *
      * @return  Response
      */
-    public function findOne($id)
+    public function findOne(Request $request, $id)
     {
+        if (method_exists($this, 'beforeFindOne')) {
+            $this->beforeFindOne($request, $id);
+        }
+
         $entity = $this->service->findOne($id);
+
+        if (method_exists($this, 'afterFindOne')) {
+            $this->afterFindOne($request, $id, $entity);
+        }
 
         if (is_null($entity)) {
             throw new HttpException(404, 'Not found');
@@ -91,7 +117,15 @@ abstract class Rest extends Base implements Interfaces\Rest
         $data = json_decode($request->getContent());
 
         try {
+            if (method_exists($this, 'beforeCreate')) {
+                $this->beforeCreate($request, $data);
+            }
+
             $entity = $this->service->create($data);
+
+            if (method_exists($this, 'afterCreate')) {
+                $this->afterCreate($request, $data, $entity);
+            }
         } catch (ValidatorException $error) {
             throw new HttpException(400, $error->getMessage());
         }
@@ -112,7 +146,15 @@ abstract class Rest extends Base implements Interfaces\Rest
         $data = json_decode($request->getContent());
 
         try {
+            if (method_exists($this, 'beforeUpdate')) {
+                $this->beforeUpdate($request, $id, $data);
+            }
+
             $entity = $this->service->update($id, $data);
+
+            if (method_exists($this, 'afterUpdate')) {
+                $this->afterUpdate($request, $id, $data, $entity);
+            }
         } catch (ValidatorException $error) {
             throw new HttpException(400, $error->getMessage());
         }
@@ -123,35 +165,150 @@ abstract class Rest extends Base implements Interfaces\Rest
     /**
      * Method to delete specified entity from database.
      *
+     * @param   Request $request
      * @param   integer $id
      *
      * @return  Response
      */
-    public function delete($id)
+    public function delete(Request $request, $id)
     {
-        $this->service->delete($id);
+        if (method_exists($this, 'beforeDelete')) {
+            $this->beforeDelete($request, $id);
+        }
+
+        $entity = $this->service->delete($id);
+
+        if (method_exists($this, 'afterDelete')) {
+            $this->afterDelete($request, $id, $entity);
+        }
 
         return $this->makeResponse('', 204);
     }
 
-    /**
-     * Helper method to make JSON response.
-     *
-     * @param   null|string|BaseEntity|BaseEntity[] $data
-     * @param   integer                             $statusCode
-     *
-     * @return  Response
-     */
-    protected function makeResponse($data, $statusCode = 200)
-    {
-        // Create new response
-        $response = new Response();
-        $response->setContent(
-            (empty($data) && !is_array($data)) ? '' : $this->app['serializer']->serialize($data, 'json')
-        );
-        $response->setStatusCode($statusCode);
-        $response->headers->set('Content-Type', 'application/json');
+     /**
+      * Before lifecycle method for find method.
+      *
+      * @param   Request        $request
+      * @param   array          $criteria
+      * @param   null|array     $orderBy
+      * @param   null|integer   $limit
+      * @param   null|integer   $offset
+      */
+     public function beforeFind(
+         Request $request,
+         array &$criteria = [],
+         array &$orderBy = null,
+         &$limit = null,
+         &$offset = null
+     ) { }
 
-        return $response;
-    }
-}
+     /**
+      * After lifecycle method for find method.
+      *
+      * @param   Request        $request
+      * @param   array          $criteria
+      * @param   null|array     $orderBy
+      * @param   null|integer   $limit
+      * @param   null|integer   $offset
+      * @param   Entity[]       $entities
+      */
+     public function afterFind(
+         Request $request,
+         array &$criteria = [],
+         array &$orderBy = null,
+         &$limit = null,
+         &$offset = null,
+         array &$entities
+     ) { }
+
+     /**
+      * Before lifecycle method for findOne method.
+      *
+      * @param   Request $request
+      * @param   integer $id
+      */
+     public function beforeFindOne(Request $request, &$id) { }
+
+     /**
+      * After lifecycle method for findOne method.
+      *
+      * @param   Request    $request
+      * @param   integer    $id
+      * @param   Entity     $entity
+      */
+     public function afterFindOne(Request $request, &$id, Entity $entity) { }
+
+     /**
+      * Before lifecycle method for create method.
+      *
+      * @param   Request   $request
+      * @param   \stdClass $data
+      */
+     public function beforeCreate(Request $request, \stdClass $data) { }
+
+     /**
+      * After lifecycle method for create method.
+      *
+      * @param   Request    $request
+      * @param   \stdClass  $data
+      * @param   Entity     $entity
+      */
+     public function afterCreate(Request $request, \stdClass $data, Entity $entity) { }
+
+     /**
+      * Before lifecycle method for update method.
+      *
+      * @param   Request   $request
+      * @param   integer   $id
+      * @param   \stdClass $data
+      */
+     public function beforeUpdate(Request $request, &$id, \stdClass $data) { }
+
+     /**
+      * After lifecycle method for update method.
+      *
+      * @param   Request    $request
+      * @param   integer    $id
+      * @param   \stdClass  $data
+      * @param   Entity     $entity
+      */
+     public function afterUpdate(Request $request, &$id, \stdClass $data, Entity $entity) { }
+
+     /**
+      * Before lifecycle method for delete method.
+      *
+      * @param   Request $request
+      * @param   integer $id
+      */
+     public function beforeDelete(Request $request, &$id) { }
+
+     /**
+      * After lifecycle method for delete method.
+      *
+      * @param   Request    $request
+      * @param   integer    $id
+      * @param   Entity     $entity
+      */
+     public function afterDelete(Request $request, &$id, Entity $entity) { }
+
+     /**
+      * Helper method to make JSON response.
+      *
+      * @param   null|string|Entity|Entity[]    $data
+      * @param   integer                        $statusCode
+      *
+      * @return  Response
+      */
+     protected function makeResponse($data, $statusCode = 200)
+     {
+         // Create new response
+         $response = new Response();
+         $response->setContent(
+             (empty($data) && !is_array($data)) ? '' : $this->app['serializer']->serialize($data, 'json')
+         );
+         $response->setStatusCode($statusCode);
+         $response->headers->set('Content-Type', 'application/json');
+
+         return $response;
+     }
+ }
